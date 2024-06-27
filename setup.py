c121bfbf7f9a3f91073bfc06c89baa98ade6d084
xmayeur <xavier@mayeur.be>
2024-06-27 15:54:02 +0200

    Added new methods

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='get_hc_secrets',
-    version='1.4',
+    version='1.5.0',
     packages=['src'],
     url='https://github.com/xmayeur/getSecrets',
     license='',
